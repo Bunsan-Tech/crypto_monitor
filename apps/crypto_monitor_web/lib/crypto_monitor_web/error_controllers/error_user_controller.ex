c71f71fada790f2db281943768f9485a0b580593
hiphoox <norberto@bunsan.io>
2017-07-25 10:06:17 -0500

    updating to Elixir 1.5 Final release

--- apps/crypto_monitor_web/lib/crypto_monitor_web/error_controllers/error_user_controller.ex
+++ apps/crypto_monitor_web/lib/crypto_monitor_web/error_controllers/error_user_controller.ex
@@ -6,13 +6,13 @@ defmodule CryptoMonitor.Web.ErrorFallBackUserController do
     changeset = User.changeset(%User{},%{})
     conn
       |> put_flash(:error, message_error)
-      |> render "bussines.html", changeset: changeset
+      |> render("bussines.html", changeset: changeset)
   end
 
   def call(conn, [wrong_pin: {message_error, []}]) do
     changeset = User.changeset(%User{},%{})
     conn
       |> put_flash(:error, message_error)
-      |> render "bussines.html", changeset: changeset
+      |> render("bussines.html", changeset: changeset)
   end
 end
