e7a8d814d57e97e55ab29e8fee0cebc98c3d8a1c
padawan <padawan@bunsan.io>
2017-07-26 18:41:56 -0500

    Added logout

--- apps/crypto_monitor_web/lib/crypto_monitor_web/views/crypto_view.ex
+++ apps/crypto_monitor_web/lib/crypto_monitor_web/views/crypto_view.ex
@@ -1,7 +1,17 @@
 defmodule CryptoMonitor.Web.CryptoView do
   use CryptoMonitor.Web, :view
+  import Plug.Conn
 
   def render_number(number) do
     "#{number}"
+  end
+
+  def render_logout(conn) do
+    case get_session(conn, :user) do
+      nil ->
+        ""
+      _ ->
+        "SALIR"
+    end
   end
 end
--- apps/crypto_monitor_web/lib/crypto_monitor_web/views/user_view.ex
+++ apps/crypto_monitor_web/lib/crypto_monitor_web/views/user_view.ex
@@ -1,4 +1,12 @@
 defmodule CryptoMonitor.Web.UserView do
   use CryptoMonitor.Web, :view
-
+  import Plug.Conn
+  def render_logout(conn) do
+    case get_session(conn, :user) do
+      nil ->
+        ""
+      _ ->
+        "SALIR"
+    end
+  end
 end
