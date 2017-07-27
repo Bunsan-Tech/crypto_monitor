42106b24094124dc8a23a2cdf6ee9b640bc22440
padawan <padawan@bunsan.io>
2017-07-26 20:54:47 -0500

    Give 10,000 usd to the user to begin his journey

--- apps/crypto_monitor/priv/repo/migrations/20170726163705_create_users.exs
+++ apps/crypto_monitor/priv/repo/migrations/20170726163705_create_users.exs
@@ -5,7 +5,7 @@ defmodule CryptoMonitor.Repo.Migrations.CreateUsers do
     create table(:users) do
       add :name, :string
       add :pin, :string
-      add :usd, :decimal, default: 0
+      add :usd, :decimal, default: 10_000 #Give the user 10,000 USD to star his journey
       add :eth, :decimal, default: 0
       add :btc, :decimal, default: 0
     end
